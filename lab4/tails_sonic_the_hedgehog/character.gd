6397e4bd0c7e2a696fbd250cac1f25ce54989d98
ameehan122 <ameehan12@fordham.edu>
2025-03-28 12:56:21 -0400

    lab4 update

--- lab4/tails_sonic_the_hedgehog/character.gd
+++ lab4/tails_sonic_the_hedgehog/character.gd
@@ -1,36 +1,34 @@
 extends CharacterBody3D
 
-@export var speed = 7.0
-@export var jump_strength = 20.0
-@export var gravity = 50.0
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
-var _velocity = Vector3.ZERO
-var _snap_vector = Vector3.DOWN
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-@onready var _spring_arm: SpringArm = $SpringArm3D
-@onready var _model: Spatial = $Sketchfab_model/b2373691094946ba92dc1410953b0dae_fbx/RootNode/Tails
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-func _physics_process(delta: float) -> void:
-	var move_direction = Vector3.ZERO
-	move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
-	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
+	move_and_slide()
 	
-	_velocity.x = move_direction.x * speed
-	_velocity.z = move_direction.z * speed
-	_velocity.y = gravity * delta
-	
-	var just_landed = is_on_floor() and _snap_vector == Vector3.ZERO
-	var is_jumping = is_on_floor() and Input.is_action_just_pressed("jump")
-	if is_jumping:
-		_velocity.y = jump_strength
-		_snap_vector = Vector3.ZERO
-	elif just_landed:
-		_snap_vector = Vector3.DOWN
-	_velocity = move_and_slide(_velocity, _snap_vector, Vector3.UP, true)
-
-func _process(delta: float) -> void:
-	_spring_arm.position = position
-	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(event.relative.x))
+		$Pivot.rotate_x(deg_to_rad(-event.relative.y))
+		$Pivot.rotation_degrees.x = clampf($Pivot.rotation_degrees.x, -90, 45)
+		print($Pivot.rotation_degrees)
